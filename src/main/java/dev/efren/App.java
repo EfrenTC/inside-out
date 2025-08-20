e91e30d1ce78309848e9546000e06fc4f9884f38
Efren TC <efrentomascampa@gmail.com>
2025-08-20 15:40:54 +0200

    feature: main actualizado con todos los cambios nuevos

--- src/main/java/dev/efren/App.java
+++ src/main/java/dev/efren/App.java
@@ -1,15 +1,27 @@
 package dev.efren;
 
 import dev.efren.controller.DiarioController;
+import dev.efren.controller.PeliculaController;
 import dev.efren.repository.MomentoRepository;
+import dev.efren.repository.PeliculaRepository;
 import dev.efren.view.ConsoleView;
 
 public class App {
     public static void main(String[] args) {
-        String storage = args.length > 0 ? args[0] : "momentos.db";
-        MomentoRepository repo = new MomentoRepository(storage);
-        DiarioController controller = new DiarioController(repo);
-        ConsoleView view = new ConsoleView(controller);
+
+        String storageMomentos = args.length > 0 ? args[0] : "momentos.db";
+        MomentoRepository momentoRepo = new MomentoRepository(storageMomentos);
+        DiarioController diarioController = new DiarioController(momentoRepo);
+
+        String storagePeliculas = args.length > 1 ? args[1] : "peliculas.csv";
+        PeliculaRepository peliculaRepo = new PeliculaRepository(storagePeliculas);
+        PeliculaController peliculaController = new PeliculaController(peliculaRepo);
+
+        ConsoleView view = new ConsoleView(diarioController, peliculaController);
+
         view.start();
+
+        diarioController.exportToCSV("momentos.csv");
+        System.out.println("CSV generado en momentos.csv");
     }
 }
